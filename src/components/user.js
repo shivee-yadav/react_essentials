0e3e67e5b7d50b40788ca6d4dc1125111d6e8911
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-03 23:04:53 +0530

    Hooks

--- src/components/user.js
+++ src/components/user.js
@@ -1,4 +1,77 @@
+//Functional Components
+import React, {useEffect,useState} from "react";
+
+const User = (props) => {
+    //value
+    //the function to update the state
+    const [planet, setPlanet] = useState("Earth");
+
+    useEffect(() => {
+        setPlanet("Jupiter");
+        console.log("Component Mounting");
+    });
+
+    console.log(planet);
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <h1>{planet}</h1>
+        </div>
+    );
+
+};
+
+export default User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//State -> a set of data that an individual component holds
+//export default User;
+
+
+//LIFE CYCLE METHODS
+//1.MOUNT -> The component is initially rendered in a DOM
+/* 
+   1.a Constructor
+   1.b Render
+   1.c ComponentDidMount<sideEffect>(get ecplore repos,activities)
+*/
+//2.UPDATE ->Component is being updated
+/*
+  2.a Constructor
+  2.b shouldComponentUpdate(nextProp, nextState)//true or false value returned
+  2.c render()
+  2.d getSnapsotBeforeUpdate(prevProp, prevState)//returns true or false
+  2.e componentDidUpdate()
+ */
+//3.UNMOUNT -> Component is removed from the DOM
+/*componentWillUnmount
+ */
+
+
 //Class Components
+/*
 import React from "react";
 class User extends React.Component {
     constructor(props) {
@@ -51,26 +124,7 @@ class User extends React.Component {
         )
     }
 }
-
-//State -> a set of data that an individual component holds
-export default User;
-
-//LIFE CYCLE METHODS
-//1.MOUNT -> The component is initially rendered in a DOM
-/* 
-   1.a Constructor
-   1.b Render
-   1.c ComponentDidMount<sideEffect>(get ecplore repos,activities)
 */
-//2.UPDATE ->Component is being updated
-/*
-  2.a Constructor
-  2.b shouldComponentUpdate(nextProp, nextState)//true or false value returned
-  2.c render()
-  2.d getSnapsotBeforeUpdate(prevProp, prevState)//returns true or false
-  2.e componentDidUpdate()
- */
-//3.UNMOUNT -> Component is removed from the DOM
 
 
 
@@ -85,19 +139,8 @@ export default User;
 
 
 
-//Functional Components
 
-// const User = (Props) => {
-//     return (
-//         <div>
-//             <h1>{Props.name}</h1>
-//             <p>{Props.description}</p>
-//         </div>
-//     );
 
-// };
-
-// export default User;
 
 //props(keyword) -> properties/parameters
 //data that has been passed from one component to another
